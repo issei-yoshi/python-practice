31857bbef520f848f96d81c0e7c78cd040a8db63
issei-yoshi <iyoshi.engineer@gmail.com>
2023-01-21 13:36:00 +0900

    Add: 13

---
+++ basic/base1.py
@@ -0,0 +1,16 @@
+# 標準入出力、コメント、変数
+
+print('Hello World')
+
+"""
+コメント
+"""
+
+name = input("what's your name? : ")
+print(name)
+age = 26
+
+print('My name is = {}, 年齢は{}'.format(name, age))
+
+a = b = 'hello'
+print(a, b)
