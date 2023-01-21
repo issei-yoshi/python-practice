b5a85cdd71bb2009f45ec349a34ce3af9c08ac4a
issei-yoshi <iyoshi.engineer@gmail.com>
2023-01-21 16:08:23 +0900

    Add: 30

---
+++ basic/base13.py
@@ -0,0 +1,23 @@
+set_a = {'a', 'b', 'c', 'd', 'a', 12}
+
+print(set_a)
+print('e' in set_a)
+print('a' in set_a)
+print('a' not in set_a)
+
+print(len(set_a))
+
+set_a.add('A')
+print(set_a)
+
+set_a.remove('b')
+print(set_a)
+
+set_a.discard(12)
+print(set_a)
+
+val = set_a.pop()
+print(val, set_a)
+
+set_a.clear()
+print(set_a)
