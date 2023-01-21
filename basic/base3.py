498858a07a048040ff9d2d3cd6aa2c0b1c3d3213
issei-yoshi <iyoshi.engineer@gmail.com>
2023-01-21 13:45:43 +0900

    Add: 15

---
+++ basic/base3.py
@@ -0,0 +1,13 @@
+is_animal = True
+
+if is_animal:
+    print('This is Animal')
+
+is_man = True
+is_adult = True
+
+if is_man or is_adult:
+    print('man or adult')
+
+if is_man and is_adult:
+    print('adultman')
