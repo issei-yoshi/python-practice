e3991ce2525366a68f45a810b83b735fbdd9e284
issei-yoshi <iyoshi.engineer@gmail.com>
2023-01-21 15:11:02 +0900

    Add: 24

---
+++ basic/base9.py
@@ -0,0 +1,12 @@
+list_a = [1, 2, 3, 4]
+list_b = []
+
+print(list_a)
+print(list_a[0])
+print(list_a[-2])
+
+
+list_a = [1, [1, 2, 'apple'], 3, 'banana']
+print(list_a[1][2])
+list_a[1][2] = 'lemon'
+print(list_a)
