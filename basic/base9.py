f413aa4ec3210b2ad11bb44f692345c4705f45a3
issei-yoshi <iyoshi.engineer@gmail.com>
2023-01-21 15:18:13 +0900

    Add: 25

--- basic/base9.py
+++ basic/base9.py
@@ -1,12 +1,29 @@
-list_a = [1, 2, 3, 4]
-list_b = []
+# list_a = [1, 2, 3, 4]
+# list_b = []
 
+# print(list_a)
+# print(list_a[0])
+# print(list_a[-2])
+
+
+# list_a = [1, [1, 2, 'apple'], 3, 'banana']
+# print(list_a[1][2])
+# list_a[1][2] = 'lemon'
+# print(list_a)
+
+list_a = [1, 2, 3, 4, 5]
+
+list_b = list_a[1:4:2]
+print(list_b)
+
+list_a.append('apple')
 print(list_a)
-print(list_a[0])
-print(list_a[-2])
 
+list_a.extend(['banana', 'melon'])
+print(list_a)
 
-list_a = [1, [1, 2, 'apple'], 3, 'banana']
-print(list_a[1][2])
-list_a[1][2] = 'lemon'
+list_a.insert(1, 'grape')
+print(list_a)
+
+list_a.clear()
 print(list_a)
