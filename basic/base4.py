426c9b978044b36627ea21e6fd07b85ea008343c
issei-yoshi <iyoshi.engineer@gmail.com>
2023-01-21 13:56:23 +0900

    Add: 16, 17

---
+++ basic/base4.py
@@ -0,0 +1,25 @@
+value = 1
+# print(value)
+# value = -2
+# print(value)
+
+# value += 4
+# print(value)
+
+# value = 10
+# print(value // 3)
+
+# print(value **3)
+
+value += 3
+
+height = 175.5
+print(height)
+print(type(height))
+
+value = 8
+print(value >> 2)
+print(value << 3)
+
+print(12 & 21)
+print(12 | 21)
