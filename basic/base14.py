93452c0f9eaa85178b493f9d1f17a66f4ce790d4
issei-yoshi <iyoshi.engineer@gmail.com>
2023-01-21 16:19:57 +0900

    Add: 31

---
+++ basic/base14.py
@@ -0,0 +1,31 @@
+s = {'a', 'b', 'c', 'd'}
+t = {'c', 'd', 'e', 'f'}
+
+u = s | t
+# u = s.union(t)
+print(u)
+
+u = s & t
+# s.intersection(t)
+print(u)
+
+u = s - t
+# u = s.difference(t)
+print(u)
+
+u = s ^ t
+# u = s.symmetric_difference(t)
+print(u)
+
+s |= t
+print(s)
+
+s = {'apple', 'banana'}
+t = {'apple', 'banana', 'lemon'}
+u = {'cherry'}
+
+print(s.issubset(t))
+print(t.issuperset(s))
+
+print(t.isdisjoint(s))
+print(t.isdisjoint(u))
