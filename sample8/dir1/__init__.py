581142b5c22f4a49cacdb56aa659b2f92fa2f2a3
issei-yoshi <iyoshi.engineer@gmail.com>
2023-01-22 23:12:02 +0900

    Add: 8-

---
+++ sample8/dir1/__init__.py
@@ -0,0 +1,6 @@
+# from . import base1
+
+from .base1 import print_msg as base1_msg
+from .base2 import print_msg as base2_msg
+
+__all__ = ['base2_msg']
--- sample8/main.py
+++ sample8/main.py
@@ -22,3 +22,30 @@
 
 
 
+# import dir1.base1 as base1
+
+# base1.print_msg()
+# # base1.print_msg()
+
+# import dir1.base2 as base2
+# base2.print_msg()
+
+# from dir1.base1 import print_msg as base1_print
+# from dir1.base2 import print_msg as base2_print
+
+# base1_print()
+# base2_print()
+
+
+#__init__
+# from dir1 import *
+# base1.print_msg()
+
+# from dir1 import base1_msg
+# base1_msg()
+
+from dir1 import *
+from dir1 import base1_msg
+
+base1_msg()
+base2_msg()
