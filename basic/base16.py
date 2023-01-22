a9da950d6bafb527e501866d232541ad67c17627
issei-yoshi <iyoshi.engineer@gmail.com>
2023-01-22 15:13:30 +0900

    Add: 34

---
+++ basic/base16.py
@@ -0,0 +1,26 @@
+# msg = 'blue'
+# if msg == 'blue':
+#     print('yes')
+# elif msg == 'red':
+#     print('stay')
+# else:
+#     print('anything else')
+
+age = 70
+if age < 20:
+    print('under 20')
+elif age <= 40:
+    print('over20, under 40')
+elif age >= 60:
+    print('over 60')
+else:
+    print('other')
+
+
+gender = 'man'
+age = 10
+if gender == 'man' and age < 20:
+    print('未成年男性')
+
+if not gender == 'woman':
+    print('女ではない')
