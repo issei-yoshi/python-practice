f372ee3f0b3f4a90b86496ea88d7c39b40488df8
issei-yoshi <iyoshi.engineer@gmail.com>
2023-01-22 16:44:46 +0900

    Add: 49

--- def/base5.py
+++ def/base5.py
@@ -1,10 +1,19 @@
 def generator(max):
     print('create generator')
     for n in range(max):
-        yield n
-        print('yield実行')
+        try:
+          x = yield n
+          print('x = {}'.format(x))
+          print('yield実行')
+        except ValueError as e:
+           print('throw実行')
 
 gen = generator(10)
+next(gen)
+gen.send(100)
+# gen.throw(ValueError('Invalid Value'))
+gen.close()
+# next(gen)
 
-for x in gen:
-    print('x = {}'.format(x))
+# for x in gen:
+#     print('x = {}'.format(x))
