ab1aadfa03e2d87976606e0c15726027d039ca81
issei-yoshi <iyoshi.engineer@gmail.com>
2023-01-22 23:01:04 +0900

    Add: 79

---
+++ sample8/main.py
@@ -0,0 +1,24 @@
+# import sys
+
+# print(sys.path)
+# sys.path.append('D:\\')
+# print(sys.path)
+
+
+
+# import sub
+# sub.sample_a()
+# class_a = sub.ClassA()
+# class_a.print_a()
+# print(sub.VAR)
+
+
+
+# from sub import sample_a as sa, ClassA as ca
+# sa()
+
+# int = ca()
+# print(type(int))
+
+
+
